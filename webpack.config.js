4c9783973b5e2cef49d305abf3da17b033a7b2dc
codermy <gsmxsky@gmail.com>
2024-04-29 14:12:39 +0800

    copy webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -28,6 +29,14 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'Execl',
             template: './src/index.html'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'about.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'public', to: 'assets' }
+            ]
         })
     ]
     // 添加其他配置项...
