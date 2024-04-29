c7a10da3864db3993d690cb5c7cc0149f681eae1
codermy <gsmxsky@gmail.com>
2024-04-29 14:03:13 +0800

    clean,html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -20,5 +22,12 @@ module.exports = {
             }
         ]
     },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Execl',
+        })
+    ]
     // 添加其他配置项...
 }
