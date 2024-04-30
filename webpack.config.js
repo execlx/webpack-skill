482f51291f0da991553f4c303a6fbdd6e91c1e53
codermy <gsmxsky@gmail.com>
2024-04-30 11:05:54 +0800

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,54 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const RemoveCommentsPlugin = require('./custom-plugin/remove-comments-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const RemoveCommentsPlugin = require("./custom-plugin/remove-comments-plugin");
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    mode: 'none',
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "none",
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.md$/i,
-                use: './md-loader'
-            }
-        ]
-    },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.md$/i,
+        use: "./md-loader",
+      },
+    ],
+  },
 
-    plugins: [
-        new webpack.ProgressPlugin(),
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Execl',
-            template: './src/index.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'about.html',
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: 'public', to: 'assets' }
-            ]
-        }),
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Execl",
+      template: "./src/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "about.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "assets" }],
+    }),
 
-        new RemoveCommentsPlugin(),
-    ]
-    // 添加其他配置项...
-}
+    // new RemoveCommentsPlugin(),
+  ],
+
+  devServer: {
+      static: {
+          directory: path.join(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 8082
+  }
+  // 添加其他配置项...
+};
